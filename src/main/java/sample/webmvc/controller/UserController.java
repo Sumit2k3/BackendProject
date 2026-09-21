88615962bfe32ffc2b61f34e6187a25b6dbf22d0
Sumit2k3 <sumitsingh2k3@gmail.com>
2026-09-21 21:01:32 +0530

    Practice @RequestParam in Spring MVC
    - Practiced using @RequestParam in a Spring MVC controller.
    - Pass the user value in the URL.
    - Mapped the request parameter to a controller method argument.
    - Practiced handling a GET request using @GetMapping.
    - Returned the home view after processing the request.
    - Created login form using JSP.
    - Added Model attributes to pass login data to the view.
    - Connected form submission to the /login endpoint using POST.

--- src/main/java/sample/webmvc/controller/UserController.java
+++ src/main/java/sample/webmvc/controller/UserController.java
@@ -1,22 +1,39 @@
 package sample.webmvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 	
-	@RequestMapping("/")
-	@ResponseBody
-	public String greet() {
-		return "Hi";
+	@GetMapping("/")
+	public String greet(@RequestParam (name="user") String user) {
+		System.out.println("UserController.greet()");
+		
+		return "home";
 	}
 	
-	@RequestMapping("/movies")
-	@ResponseBody
-	public String movie() {
-		return "Hi";
-	
+	@GetMapping("/login")
+	public String login() {
+		
+		System.out.println("UserController.login()");
+		
+		return "login";
 	}
+	
+	@PostMapping("/login")
+	public String userlogin(@RequestParam (name="username") String username, @RequestParam (name="password") String password, Model model) {
+		
+		System.out.println("UserController.userlogin : " + username);
+		System.out.println("UserController.userlogin : " + password);
+		
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		
+		return "profile";
+	}
+	
 }
